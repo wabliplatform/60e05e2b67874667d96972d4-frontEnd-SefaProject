cc6d007dc544c38301cd3e74fdf8837aa7d1b3a7
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-02-17 17:10:47 +0200

    My commit message

--- javascript/Overview.js
+++ javascript/Overview.js
@@ -18,6 +18,8 @@ let apiProjectApi = new TempApi.ProjectApi();import TempApi from '../src/index';
     event.preventDefault();
     { location.href= '/Profit&Loss';}};document.getElementById('iix52a').onclick = (event) => {
     event.preventDefault();
-    { location.href= '/RiskAssessmentProtocol';}};document.getElementById('ikd58b').onclick = (event) => {
+    { location.href= '/RiskAssessmentProtocol';}};document.getElementById('ir7zdp').onclick = (event) => {
+    event.preventDefault();
+    { location.href= '/HomePage';}};document.getElementById('ikd58b').onclick = (event) => {
     event.preventDefault();
     project['projectName'] = document.querySelector("[annotationname = 'projectName']").value;project['projectDescription'] = document.querySelector("[annotationname = 'projectDescription']").value;apiProjectApi.createproject( project, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); { location.href= '/Contract/'+response.body.query._id+'';}}});};window.onload = () => {};
