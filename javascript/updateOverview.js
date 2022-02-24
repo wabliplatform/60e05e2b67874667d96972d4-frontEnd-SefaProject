26b79d4a24513f1bc1785002b59fad340a40e25f
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-02-24 18:46:51 +0200

    My commit message

--- javascript/updateOverview.js
+++ javascript/updateOverview.js
@@ -46,4 +46,14 @@ let apiProjectApi = new TempApi.ProjectApi();import TempApi from '../src/index';
           }
         })
       }
-   let project = new TempApi.Project();project['projectName'] = document.querySelector("[annotationname = 'projectName']").value;project['projectDescription'] = document.querySelector("[annotationname = 'projectDescription']").value; let opts = {project};apiProjectApi.updateproject( projectId, opts, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); document.querySelector('[annotationname = projectName]').value = response.body.query.projectName ;document.querySelector('[annotationname = projectDescription]').value = response.body.query.projectDescription ;{ location.href= '/Contract/'+response.body.query._id+'';}}});};window.onload = () => {};
+   let project = new TempApi.Project();project['projectName'] = document.querySelector("[annotationname = 'projectName']").value;project['projectDescription'] = document.querySelector("[annotationname = 'projectDescription']").value; let opts = {project};apiProjectApi.updateproject( projectId, opts, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); document.querySelector('[annotationname = projectName]').value = response.body.query.projectName ;document.querySelector('[annotationname = projectDescription]').value = response.body.query.projectDescription ;{ location.href= '/Contract/'+response.body.query._id+'';}}});};window.onload = () => {let projectId = window.location.pathname.replace('/updateOverview/','');
+      if(projectId === '/updateOverview' || projectId === ''){
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach( (value, key) => {
+          if(document.getElementById(key).contains(document.getElementById('isw2as')) === true){
+            projectId = value._id;
+          }
+        })
+      }
+   apiProjectApi.getproject( projectId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); document.querySelector('[annotationname = projectName]').value = response.body.query.projectName ;document.querySelector('[annotationname = projectDescription]').value = response.body.query.projectDescription ;}});};
